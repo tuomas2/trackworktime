5766c28e64eff8eb215cb83b0373900b9cbd3aeb
Tuomas Airaksinen <tuomas.airaksinen@gisgro.com>
2026-06-04 21:29:49 +0300

    Add EventOrigin.PEBBLE for watch-initiated clocking

--- app/src/main/java/org/zephyrsoft/trackworktime/timer/TimerManager.java
+++ app/src/main/java/org/zephyrsoft/trackworktime/timer/TimerManager.java
@@ -74,7 +74,8 @@ public class TimerManager {
 		/** this includes both externally created broadcasts and actions from TWT's own widget and notification */
 		RECEIVED_INTENT,
 		/** events with this source are NOT sent out as broadcasts! */
-		RESTORE_BACKUP
+		RESTORE_BACKUP,
+		PEBBLE
 	}
 
 	private final DAO dao;
